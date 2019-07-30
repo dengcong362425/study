74858f0ea6c893d7116bedc17e5d2b3ffc1acd13
cong.deng <cong.deng@canacorp.net>
2019-07-30 09:57:31 +0800

    submit

---
+++ submit.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+function branch {
+  br=`git branch | grep "*"`
+  echo ${br/* /}
+}
+var=$(branch)
+echo `git add .`
+echo `git commit -m $1`
+echo `git fetch && git rebase` 
+echo `git push origin  $var`
